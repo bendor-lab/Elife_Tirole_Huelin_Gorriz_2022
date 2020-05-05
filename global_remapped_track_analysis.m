386577d9725765d51a9668f93fa13ac48d8cd3dc
martahuelin <43796540+martahuelin@users.noreply.github.com>
2020-05-05 13:58:28 +0100

    Updated global remapping

--- global_remapped_track_analysis.m
+++ global_remapped_track_analysis.m
@@ -1,14 +1,16 @@
-function  global_remapped_track_analysis
+function  global_remapped_track_analysis(RExp)
+
+
 load extracted_position.mat
 load extracted_replay_events.mat
 load extracted_sleep_state.mat
 load extracted_clusters.mat
+load extracted_place_fields_BAYESIAN.mat
+load extracted_place_fields.mat
 
 if exist('global_remapped')~=7
     mkdir global_remapped
 end
-create_global_remapped_track('BAYESIAN');
-create_global_remapped_track('NORMAL');
 
 %copy relevant data from main folder to chimeric folder
 cd global_remapped
@@ -16,9 +18,14 @@ save extracted_position position
 save extracted_replay_events replay reactivations
 save extracted_sleep_state sleep_state
 save extracted_clusters clusters
+save extracted_place_fields_BAYESIAN.mat
+save extracted_place_fields.mat
 
-batch_analysis('REPLAY');  %run batch_analysis for decoding replay and running shuffles
-batch_analysis('ANALYSIS');
+create_global_remapped_track('BAYESIAN');
+create_global_remapped_track('NORMAL');
+
+remapping_pipeline('REPLAY',RExp);  %run batch_analysis for decoding replay and running shuffles
+remapping_pipeline('ANALYSIS',RExp);
 cd ..
 end
 
